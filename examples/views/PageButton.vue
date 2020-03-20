7ab5199746a02bf146bc6280fd5ff656b33f7553
i-flower <https://github.com/i-flower/git-.git>
2020-03-20 22:54:31 +0800

    设置loading状态、设置animated动画

--- examples/views/PageButton.vue
+++ examples/views/PageButton.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <!-- 图标大小和自定义内容测试用例 -->
+    <!-- 设置按钮动画测试用例 -->
+    <my-button animated="vertical">
+      <div slot="hidden">$10000</div>
+      <div slot="visible">
+       <i class="shop icon"></i> 不要错过o
+      </div>
+    </my-button>
+    <br>
+    <my-button animated="fade">
+      <div slot="hidden">$10000</div>
+      <div slot="visible">不要错过哈
+      </div>
+    </my-button>
+    <br>
+    <!-- 设置按钮大小和自定义内容测试用例 -->
     <my-button size="mini">点个赞再走</my-button>
     <br>
     <my-button></my-button>
     <br>
     <my-button size="huge">hehehehe</my-button>
     <br>
-    <!-- 图标测试用例 -->
+    <!-- 设置图标测试用例 -->
     <my-button icon="wifi icon">带图标的按钮</my-button>
     <br>
     <my-button icon="user icon" size="big">带图标的按钮</my-button>
-    <!-- 图标禁用测试用例 -->
+    <!-- 设置按钮禁用测试用例 -->
     <br>
     <my-button>没有设置禁用的按钮</my-button>
     <br>
     <my-button disabled icon="users icon">被禁用的按钮</my-button>
+    <!-- 设置按钮加载效果测试用例 -->
+    <br>
+    <my-button loading>设置loading的按钮</my-button>
   </div>
 </template>
--- packages/button/button.vue
+++ packages/button/button.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-     <button class="ui primary button" :class="cClass">
+    <button class="ui primary button" :class="cClass">
+      <!-- 设置按钮动画组要进行判断 -->
+      <!-- template的用法。它不需要创建新的dom容器，还可以用来包裹其它元素。 -->
+      <template v-if="animated">
+        <!-- 用$slots.插槽名来获取指定插槽的内容。
+             hidden content 是在semantic-ui中约定的类
+             visible conten 是在semantic-ui中约定的类-->
+        <div v-if="$slots.hidden" class="hidden content">
+          <!-- 具名插槽 -->
+          <slot name="hidden"></slot>
+        </div>
+        <div v-if="$slots.visible" class="visible content">
+          <slot name="visible"></slot>
+        </div>
+      </template>
+      <template v-else>
+        <!-- 不带动画效果的按钮 -->
+        <!-- 如果用户设置了icon属性，则显示图标 -->
         <i v-if="icon" class="icon" :class="icon"></i>
         <!-- slot是默认插槽
              会把默认内容替换成<my-button>点个赞再走</my-button> "点个赞再走"
-             如果用户没有在组件中输入内容，就会显示插槽中的默认内容-->
+        如果用户没有在组件中输入内容，就会显示插槽中的默认内容-->
         <slot>我是默认内容</slot>
-     </button>
+      </template>
+    </button>
   </div>
 </template>
 
@@ -18,14 +36,23 @@ export default {
       type: String,
       default: 'medium',
       validator (val) {
-      // validator是校验规则
-      // 只要用户设置按钮大小，传入size属性，就会进入到这个函数
-      // 如果校验结果返回true就生效，如果校验结返回false就不允许，会报错
+        // validator是校验规则
+        // 只要用户设置按钮大小，传入size属性，就会进入到这个函数
+        // 如果校验结果返回true就生效，如果校验结返回false就不允许，会报错
 
         // 检验思路：
         // 如果传入的size值为：mini,medium,huge,massive... 就成功；否则为false
         // Array.includes() 这个数组方法是检测这个数组中，是否包含某个元素
-        return ['mini', 'tiny', 'small', 'medium', 'large', 'big', 'huge', 'massive'].includes(val)
+        return [
+          'mini',
+          'tiny',
+          'small',
+          'medium',
+          'large',
+          'big',
+          'huge',
+          'massive'
+        ].includes(val)
       }
     },
     icon: {
@@ -36,22 +63,48 @@ export default {
       type: Boolean,
       // 设置按钮默认不是禁用状态
       default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    animated: {
+      type: String,
+      default: ''
     }
   },
   computed: {
     cClass () {
-      // 手机收到的用户设置的属性值，并分析，然后返回class给按钮外层的容器
-      // 因为size、disabled都需要动态添加类名，disabled不是必须添加的，所以我们要使用计算属性
-      if (this.disabled) {
-        console.log(this.disabled)
+      // 声明一个数组接收用户设置的属性值
+      const classArr = [this.size]
+      // 判断用户是否有设置animated类,如果有设置需要将动画类名push到classArr数组
+      // 并且要将动画的类型，也就是动画名称同时push进去
+      this.animated && classArr.push(`animated ${this.animated}`)
+      // 判断用户是否设置loading加载状态，如果有设置向数组内追加一个类名
+      this.loading && classArr.push('loading')
+      // 判断用户是否设置禁用状态，如果有设置向数组内追加一个类名
+      this.disabled && classArr.push('disabled')
 
-        // 如果用户开启禁用
-        // 这里返回的是class类名，加空格是使得两个类名隔开
-        return this.size + ' ' + 'disabled'
-      } else {
-        // 如果用户没有开启禁用，只需返回size属性值
-        return this.size
-      }
+      // 以下代码过于繁琐，不简洁，进行代码优化
+      // if (this.loading) {
+      //   classArr.push('loading')
+      // }
+      // // 手机收到的用户设置的属性值，并分析，然后返回class给按钮外层的容器
+      // // 因为size、disabled都需要动态添加类名，disabled不是必须添加的，所以我们要使用计算属性
+      // if (this.disabled) {
+      //   // console.log(this.disabled)
+
+      //   // 如果用户开启禁用
+      //   // 这里返回的是class类名，加空格是使得两个类名隔开
+      //   // return this.size + ' ' + 'disabled'
+      //   classArr.push('disabled')
+      // }
+      // else {
+      //   // 如果用户没有开启禁用，只需返回size属性值
+      //   return this.size
+      // }
+      // .join() 是数组方法，将数组转换成一个字符串，并返回一个字符串，用什么隔开，括号内就写什么
+      return classArr.join(' ')
     }
   },
   data () {
